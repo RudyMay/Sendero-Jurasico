b81c198c71634454bd21e78754b4b18fb51e3456
RudyMay <rudy_0123@hotmail.com>
2022-04-21 19:37:11 -0500

    bug de flecha sobrepuesta fix

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,28 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
+const flechas = document.querySelectorAll('.flecha')
 
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    
+
+    if(hamburger.classList.contains("active")){
+        flechas.forEach(element => {
+            element.classList.remove("bounce2")
+        });
+       
+    }else{
+       flechas.forEach(element => {
+           element.classList.add("bounce2")
+       });
+        
+    }
 })
 
 document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=>{
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    
 }))
+
