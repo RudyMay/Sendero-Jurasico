836a584c3de6ffc2a735ce22baed85a62b020de9
RudyMay <rudy_0123@hotmail.com>
2022-04-29 21:47:20 -0500

    ajustes pantallas aun mas pequeñas

--- javascript/main.js
+++ javascript/main.js
@@ -26,3 +26,34 @@ document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=
     
 }))
 
+if(window.innerHeight<=680){
+    console.log('menor');
+
+    document.querySelector('#Intro div p').style="font-size:18px";
+    document.querySelector('#Intro div h2').style="font-size:28px";
+   const seccionesh2= document.querySelectorAll('section div h2')
+    const seccionesspan= document.querySelectorAll('section div h2 span')
+    const seccionesh3 =document.querySelectorAll('section div h3')
+    const seccionesp = document.querySelectorAll('section div p')
+   const botones= document.querySelectorAll('.playbtn')
+    
+   for(let sect4 of seccionesspan){
+        sect4.style.fontSize= "38px";
+    }
+
+    for(let sect of seccionesh2 ){
+        sect.style.fontSize="24px";
+    }
+    
+    for(let sect2 of seccionesh3){
+        sect2.style.fontSize= "20px";
+    }
+    
+    for(sect3 of seccionesp){
+        sect3.style.fontSize="16px";
+    }
+
+   for(let boton of botones){
+       boton.style.width="50px";
+   }
+}
